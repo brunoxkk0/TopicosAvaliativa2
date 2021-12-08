ffaed1b13f1d2f550b60806a932717a6872ba68e
Bruno <cocacola_bruno@hotmail.com>
2021-12-08 00:51:28 -0300

    Commit 2

--- src/server.js
+++ src/server.js
@@ -3,12 +3,16 @@ const bodyParser = require('body-parser');
 const uuid = require("uuid").v4;
 
 const noticias = [];
+const inscricao_emails = [];
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+async function getNoticia(id){
+    return noticias.find(noticia => noticia.id === id);
+}
 
 async function postNoticia(noticia){
     noticia.id = uuid();
@@ -23,6 +27,54 @@ async function existsNoticia(noticia){
         news.url    === noticia.url
     )
 }
+
+async function existsEmail(email){
+    return await inscricao_emails.find(em =>
+        em.email === email.email
+    )
+}
+
+
+async function registerEmail(email){
+    inscricao_emails.push(email)
+}
+
+app.post("/inscricao", async (req, res) => {
+
+    const email = req.body;
+
+    if(!email.email){
+        res.status(500).send("Email inválido.")
+        return;
+    }
+
+    if(await existsEmail(email)){
+        res.status(500).send("Este email já foi cadastrado.")
+        return;
+    }
+
+    await registerEmail(email)
+    res.send("Email " + email.email + " cadastrado.")
+});
+
+app.post("/noticia", async (req, res) => {
+
+    const noticia = req.body;
+
+    if(!(noticia.title && noticia.resumo && noticia.url)){
+        res.status(500).send("Notícia inválida.")
+        return;
+    }
+
+    if(await existsNoticia(noticia)){
+        res.status(500).send("Esta notícia já existe.")
+        return;
+    }
+
+    const id = await postNoticia(noticia);
+    res.send("Notícia adicionada. ID:" + id)
+});
+
 
 app.post("/noticia", async (req, res) => {
 
@@ -47,7 +99,24 @@ app.get("/noticia", async (req, res) => {
     res.status(500).send("Não existem notícias cadastradas.");
 });
 
+app.get("/noticia/:id", async (req, res) => {
+
+    const noticia_id = req.params.id;
+
+    if (noticia_id) {
+
+        const noticia = await getNoticia(noticia_id);
+
+        if (noticia) {
+            res.status(200).send(noticia);
+            return;
+        }
+
+    }
+
+    res.status(500).send("This request is invalid.");
+});
+
 app.listen(1234, () => {
     console.log("Servidor ligado na porta: 1234")
 })
-
